e980031944c6eebf9242a27e92aafa66126f60a4
dev <legendaryje@gmail.com>
2024-05-12 13:17:39 +0300

    JEST

---
+++ __tests__/index.test.js
@@ -0,0 +1,11 @@
+import reverse from '../src/index.js';
+
+test('reverse', () => {
+  const str = 'hellO';
+  expect(reverse(str)).toEqual('olleh');
+  expect(reverse('')).toEqual('');
+});
+
+/** Jest поддерживает ECMAScript модули в экспериментальном режиме
+ Чтобы активировать поддержку модулей в тестах,
+ нужно запускать тесты с переменной окружения NODE_OPTIONS */
